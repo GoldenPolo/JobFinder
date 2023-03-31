9e1d07e1361cb78b1200238987c193a2ba1d66ed
Pol Corty <pol.corty@etu.utc.fr>
2023-03-31 17:33:43 +0200

    premiers modeles

---
+++ TD3/organisations.js
@@ -0,0 +1,32 @@
+var db = require('./db.js'); 
+
+module.exports = {
+    read: function (siren, callback) {
+        db.query("select * from Organisation where siren = ?", email, function (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+
+    readall: function (callback) {
+        db.query("select * from Organisation", function (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    },
+
+    create: function (siren, nom, type, siege, callback) {
+        db.query("insert into Organisation(siren, nom, type, siege, validee) values(?, ?, ?, ?, ?)", [siren, nom, type, siege, 0], function (err, results) {
+            if (err) throw err
+            callback(results);
+        });
+    },
+
+    delete: function (siren, callback) {
+        db.query("delete from Organisation where siren = ?", siren, function (err, results) {
+            if (err) throw err;
+            callback(results);
+        });
+    }
+
+}
