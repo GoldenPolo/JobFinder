191a713900ea87a3e8b87b05c646bd3f30f9be3b
Romane Kulesza <romane.kulesza@etu.utc.fr>
2023-04-07 15:25:35 +0000

    Update TD3/myapp/routes/recruter.js, TD3/myapp/model/utilisateur.js

--- TD3/myapp/routes/recruter.js
+++ TD3/myapp/routes/recruter.js
@@ -6,10 +6,26 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/userslist', function (req, res, next) { 
-  result = userModel.readall(function(result){
-    res.render('usersList', { title: 'List des utilisateurs', users: result });
+router.get('/offersList', function (req, res, next) { 
+  result = offerModel.readOffresOrganisation(function(result){
+    res.render('offersList', { title: 'Offres', offers: result });
   });
+});
+
+router.get('/offerDetails', function (req, res, next) { 
+  result = offerModel.read(req.query.id, function(result){
+    res.render('offerDetails', { title: 'Détails de l\'offre', offer: result });
+  });
+});
+
+router.get('/myApplications', function (req, res, next) {
+  result = CandidaturesModel.readall(req.session.user.id, function(result){
+    res.render('myApplications', { title: 'Mes candidatures', applications: result });
+  });
+});
+
+router.get('/addOrganisation', function (req, res, next) {
+  res.render('addOrganisation', { title: 'Ajouter mon organisation' });
 });
 
 module.exports = router;
