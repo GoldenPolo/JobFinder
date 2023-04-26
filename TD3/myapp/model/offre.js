9c7af3318e0fddad0c3ac6ae2b9b0345518987ef
Pol Corty <pol.corty@etu.utc.fr>
2023-04-26 15:26:50 +0200

    continuer offerDetails

--- TD3/myapp/model/offre.js
+++ TD3/myapp/model/offre.js
@@ -30,7 +30,7 @@ module.exports = {
     },
 
     read: function (id, callback) {
-        db.query("select Offre.etat as offre_etat, Offre.dateValidite as offre_date_validite, Offre.nombrePiecesDemandees as offre_nombre_pieces_demandees, Offre.indications as offre_indications, Organisation.nom as organisation_nom, Organisation.type as organisation_type, Organisation.siege as organisation_siege, FichePoste.intitule as fiche_intitule, FichePoste.statut as fiche_statut, FichePoste.responsable as fiche_responsable, FichePoste.type as fiche_type, FichePoste.description as fiche_description, FichePoste.lieu as fiche_lieu, FichePoste.rythme as fiche_rythme, FichePoste.salaireMin as fiche_salaire_min, FichePoste.salaireMax as fiche_salaire_max from (Offre INNER JOIN Organisation ON (Offre.organisation = Organisation.siren) INNER JOIN FichePoste ON (Offre.fichePoste = FichePoste.id)) WHERE (Offre.id = ?)", [id], function (err, results) {
+        db.query("select Offre.id, Offre.dateValidite, Organisation.nom, FichePoste.intitule, FichePoste.statut, FichePoste.type, FichePoste.lieu, FichePoste.rythme, FichePoste.salaireMin, FichePoste.salaireMax from (Offre INNER JOIN Organisation ON (Offre.organisation = Organisation.siren) INNER JOIN FichePoste ON (Offre.fichePoste = FichePoste.id)) WHERE (Offre.id = ?)", [id], function (err, results) {
             if (err) throw err;
             callback(results[0]);
         });
